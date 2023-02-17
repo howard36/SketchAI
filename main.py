4135acfe5cdd05552ca7691bae40520489f13a57
Howard Halim <howardhalim@gmail.com>
2023-02-16 19:21:12 -0500

    add loss for far away points

--- main.py
+++ main.py
@@ -7,50 +7,44 @@ import matplotlib.pyplot as plt
 from similarity import similarity
 from render import render_strokes
 
+GRID_SZ = 224
+
 # check for consecutive points
-# stroke = torch.rand((10, 2), dtype=torch.float, requires_grad=True)
-# stroke = torch.tensor([(0.3, 0.3), (0.3, 0.7), (0.7, 0.7), (0.7,0.3), (0.2, 0.2)], requires_grad=True)
+stroke = torch.rand((50, 2), dtype=torch.float, requires_grad=True)
+# stroke = torch.tensor([(0.3, 0.3), (0.3, 0.7), (0.7, 0.7), (0.7,0.3), (0.2, 0.2), (0.3, 0.7), (0.7, 0.7), (0.7,0.3)], requires_grad=True)
 # target = stroke_to_picture([torch.tensor([(0,0), (99,99)], dtype=torch.float)])
 
 
-GRID_SZ = 224
-# target = []
-# for i in range(GRID_SZ):
-#     target.append([])
-#     for j in range(GRID_SZ):
-#         if abs(math.sqrt((i-GRID_SZ/2)**2 + (j-GRID_SZ/2)**2) - GRID_SZ/3) < 5:
-#             target[i].append(1.0)
-#         else:
-#             target[i].append(0.0)
+target = []
+for i in range(GRID_SZ):
+    target.append([])
+    for j in range(GRID_SZ):
+        d = abs(math.sqrt((i-GRID_SZ/2)**2 + (j-GRID_SZ/2)**2) - GRID_SZ/3)
+        target[i].append(min(d/20, 1))
+target = torch.tensor(target)
 
-# target = torch.tensor(target)
+# grid = torch.ones((GRID_SZ, GRID_SZ, 3), requires_grad=True)
 
-grid = torch.ones((GRID_SZ, GRID_SZ, 3), requires_grad=True)
-
-optimizer = optim.Adam([grid], lr=0.1)
+optimizer = optim.Adam([stroke], lr=0.02)
 
 plt.ion()
-# grid = target
+# grid = torch.ones((GRID_SZ, GRID_SZ))
+grid = target
 fig, ax = plt.subplots()
 axim = ax.imshow(grid.detach().numpy())
 
 for i in range(1000):
     optimizer.zero_grad()
-    # grid = render_strokes([stroke], GRID_SZ, 5)
+    grid = render_strokes([stroke], GRID_SZ, 5)
+    # print(grid.sum())
     # grid = torch.stack((grid,grid,grid), dim=2)
-    # loss = -torch.sum(grid * target)
-    # loss = torch.linalg.norm(grid-target)**2
-    grid.retain_grad()
-    # transform = T.Resize((GRID_SZ, GRID_SZ))
-    # larger_grid = transform(grid)
-    loss = similarity("a photo of a cat", grid)
+    # grid.retain_grad()
+    loss = torch.linalg.norm(grid-target)**2 + 0.001*GRID_SZ*GRID_SZ*torch.linalg.norm(stroke-0.5)**2
+    # loss = similarity("a sketch of a cat", grid)
     loss.backward()
-    # print(stroke.grad)
-    # print(grid.requires_grad)
-    # print(grid.grad)
     optimizer.step()
 
     axim.set_data(grid.detach().numpy())
     fig.canvas.flush_events()
-    print(loss.item())
+    print(f'Loss: {loss.item()}')
  
