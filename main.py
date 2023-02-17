e6a743e3034558a0e28a4e69bfdb350e545793f9
ldcWV <ldorianchen@gmail.com>
2023-02-16 20:24:12 -0500

    use cuda

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import torchvision.transforms as T
 import matplotlib.pyplot as plt
 
 from similarity import similarity
-from render import render_strokes
+from render import StrokeRenderer
 
 GRID_SZ = 224
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
 # check for consecutive points
-stroke = torch.rand((50, 2), dtype=torch.float, requires_grad=True)
+stroke1 = torch.rand((50, 2), dtype=torch.float, requires_grad=True)
 # stroke = torch.tensor([(0.3, 0.3), (0.3, 0.7), (0.7, 0.7), (0.7,0.3), (0.2, 0.2), (0.3, 0.7), (0.7, 0.7), (0.7,0.3)], requires_grad=True)
 # target = stroke_to_picture([torch.tensor([(0,0), (99,99)], dtype=torch.float)])
 
@@ -22,20 +25,25 @@ for i in range(GRID_SZ):
         d = abs(math.sqrt((i-GRID_SZ/2)**2 + (j-GRID_SZ/2)**2) - GRID_SZ/3)
         target[i].append(min(d/20, 1))
 target = torch.tensor(target)
+target = target.to(device)
+
+renderer = StrokeRenderer(GRID_SZ, 5)
+renderer.to(device)
 
 # grid = torch.ones((GRID_SZ, GRID_SZ, 3), requires_grad=True)
 
-optimizer = optim.Adam([stroke], lr=0.02)
+optimizer = optim.Adam([stroke1], lr=0.02)
 
 plt.ion()
 # grid = torch.ones((GRID_SZ, GRID_SZ))
 grid = target
 fig, ax = plt.subplots()
-axim = ax.imshow(grid.detach().numpy())
+axim = ax.imshow(grid.cpu().detach().numpy())
 
 for i in range(1000):
     optimizer.zero_grad()
-    grid = render_strokes([stroke], GRID_SZ, 5)
+    stroke = stroke1.to(device)
+    grid = renderer([stroke])
     # print(grid.sum())
     # grid = torch.stack((grid,grid,grid), dim=2)
     # grid.retain_grad()
@@ -44,7 +52,7 @@ for i in range(1000):
     loss.backward()
     optimizer.step()
 
-    axim.set_data(grid.detach().numpy())
+    axim.set_data(grid.cpu().detach().numpy())
     fig.canvas.flush_events()
     print(f'Loss: {loss.item()}')
  
--- render.py
+++ render.py
@@ -1,35 +1,43 @@
 import torch
+from torch import nn
+from main import device
 
-def render_strokes(strokes, G, K):
-    grid = torch.ones((G,G), dtype=torch.float)
-    for stroke in strokes:
-        grid = torch.min(grid, render_stroke(stroke * G, G, K))
-    return grid # G x G
+class StrokeRenderer(nn.Module):
+    def __init__(self, G, K):
+        super(StrokeRenderer, self).__init__()
+        self.G = G
+        self.K = K
+        idxs = torch.arange(G)
+        x_coords, y_coords = torch.meshgrid(idxs, idxs, indexing='ij') # G x G
+        self.grid_coords = torch.stack((x_coords, y_coords), dim=2).reshape(1,G,G,2).to(device) # 1 x G x G x 2
+    
+    def forward(self, strokes):
+        grid = torch.ones((self.G, self.G), dtype=torch.float)
+        grid = grid.to(device)
+        for stroke in strokes:
+            grid = torch.min(grid, self.render_stroke(stroke * self.G))
+        return grid # G x G
 
-def render_stroke(stroke, G, K):
-    n = len(stroke)
-    vs = stroke[:-1].reshape((-1,1,1,2)) # (n-1) x 1 x 1 x 2
-    vs = torch.tile(vs, (1, G, G, 1)) # (n-1) x G x G x 2
+    def render_stroke(self, stroke):
+        n = len(stroke)
+        vs = stroke[:-1].reshape((-1,1,1,2)) # (n-1) x 1 x 1 x 2
+        vs = torch.tile(vs, (1, self.G, self.G, 1)) # (n-1) x G x G x 2
 
-    ws = stroke[1:].reshape((-1,1,1,2)) # (n-1) x 1 x 1 x 2
-    ws = torch.tile(ws, (1, G, G, 1)) # (n-1) x G x G x 2
+        ws = stroke[1:].reshape((-1,1,1,2)) # (n-1) x 1 x 1 x 2
+        ws = torch.tile(ws, (1, self.G, self.G, 1)) # (n-1) x G x G x 2
 
-    idxs = torch.arange(G)
-    x_coords, y_coords = torch.meshgrid(idxs, idxs, indexing='ij') # G x G
-    coords = torch.stack((x_coords, y_coords), dim=2).reshape(1,G,G,2) # 1 x G x G x 2
-    coords = torch.tile(coords, (n-1, 1, 1, 1)) # (n-1) x G x G x 2 
+        coords = torch.tile(self.grid_coords, (n-1,1,1,1)) # (n-1) x G x G x 2
+        distances = self.dist_line_segment(coords, vs, ws) # (n-1) x G x G
+        darkness = distances/self.K # (n-1) x G x G
+        return torch.min(darkness, dim=0).values # G x G
 
-    distances = dist_line_segment(coords, vs, ws) # (n-1) x G x G
-    darkness = distances/K # (n-1) x G x G
-    return torch.min(darkness, dim=0).values # G x G
-
-# distance from point p to line segment v--w
-def dist_line_segment(p, v, w):
-    d = torch.linalg.norm(v-w, dim=3) # (n-1) x G x G
-    dot = (p-v) * (w-v)
-    dot_sum = torch.sum(dot, dim=3) / (d**2 + 1e-5)
-    t = dot_sum.unsqueeze(3) # (n-1) x G x G x 1
-    t = torch.clamp(t, min=0, max=1) # (n-1) x G x G
-    proj = v + t * (w-v) # (n-1) x G x G x 2
-    return torch.linalg.norm(p-proj, dim=3)
-   
+    # distance from point p to line segment v--w
+    def dist_line_segment(self, p, v, w):
+        d = torch.linalg.norm(v-w, dim=3) # (n-1) x G x G
+        dot = (p-v) * (w-v)
+        dot_sum = torch.sum(dot, dim=3) / (d**2 + 1e-5)
+        t = dot_sum.unsqueeze(3) # (n-1) x G x G x 1
+        t = torch.clamp(t, min=0, max=1) # (n-1) x G x G
+        proj = v + t * (w-v) # (n-1) x G x G x 2
+        return torch.linalg.norm(p-proj, dim=3)
+    
